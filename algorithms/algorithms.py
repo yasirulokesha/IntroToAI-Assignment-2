ec0cd2b63689097909f43bf28737bf5b8acf9c8c
Prawud Rathnayake <56180351+PrawudR@users.noreply.github.com>
2025-04-09 08:53:22 +1000

    algorithms.py

--- algorithms/algorithms.py
+++ algorithms/algorithms.py
@@ -1,4 +1,18 @@
 from algorithms.depth_first_search import dfs_fun
+from algorithms.breadth_first_search import bfs_fun
+from algorithms.a_star import a_star_fun
+from algorithms.greedy_best_first_search import gbfs_fun
 
 def dfs(problem):
     dfs_fun(problem)
+
+def bfs(problem):
+    bfs_fun(problem)
+
+def a_star(problem):
+    a_star_fun(problem)
+
+def gbfs(problem):
+    gbfs_fun(problem)
+
+
